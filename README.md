33b05b3da14ff24751411bfb5d1fc0867e11ea6b
khimaros <me@khimaros.com>
2021-12-31 21:23:34 -0800

    improve reliability and fallback behavior
    - use Levenstein distance for title comparisons
    - only restore one window to a particular saved window slot
    - handle sequential windows with the same title in a sane way
    - increase the restore delay to 4000ms to improve Firefox restore

--- README.md
+++ README.md
@@ -8,17 +8,13 @@ smart-auto-move is a Gnome Shell extension which keeps track of all application 
 
 window locations are not currently persisted to disk. when the extension is restarted (eg. on logout), window positions will need to be retrained.
 
-there is a fixed delay of 2000ms between when a window is created and when smart-auto-move restores its position. this is due to a limitation of the `windows-changed` signal. changing a window's frame rect inside of this handler does not actually move the window, so it instead needs to happen in the Mainloop.
+there is a fixed delay of 4000ms between when a window is created and when smart-auto-move restores its position. this is due to a limitation of the `windows-changed` signal. changing a window's frame rect inside of this handler does not actually move the window, so it instead needs to happen in the Mainloop. rather than using a fixed delay before restoring a window, it may be possible to poll the window's frame rect to determine if it is ready to be moved. this may be as simple as checking if the rect is currently set to all `0` values.
 
-if a window's title changes even slightly across restart, the window will be forgotten. this is especially problematic for eg. GMail tabs where the unread count changes the title of the window.
+## behavior
 
-if you have multiple windows with the same title, they will restore to the same exact location rather than using startup sequence as a secondary hint. this is problematic for eg. terminal windows which often start with the same title.
+because there is no way to uniquely distinguish individual windows from an application across restarts, smart-auto-move needs a heuristic to uniquely identify them. this is primarily based on startup order and title. in cases where there are multiple windows with the same title, they are restored based on relative startup sequence.
 
-## future improvements
-
-because there is no way to uniquely distinguish individual windows from an application across restarts, smart-auto-move needs a heuristic to uniquely identify them. this should be based on startup order and title. in cases where there are multiple windows with the same title, they should be restored based on relative startup sequence.
-
-rather than using a fixed delay before restoring a window, it may be possible to poll the window's frame rect to determine if it is ready to be moved. this may be as simple as checking if the rect is currently set to all `0` values.
+titles are matched using Levenstein distance. the match bonus for title is calculated based on `(title length - distance) / title length`.
 
 ## installation
 
--- smart-auto-move@khimaros.com/extension.js
+++ smart-auto-move@khimaros.com/extension.js
@@ -8,7 +8,8 @@ const St = imports.gi.St;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-const WINDOW_RESTORE_DELAY_MS = 2000;
+const WINDOW_RESTORE_DELAY_MS = 4000;
+const WINDOW_RESTORE_MATCH_THRESHOLD = 0.7;
 
 let appSystem = Shell.AppSystem.get_default();
 
@@ -55,6 +56,8 @@ function windowReady(win) {
 function windowToSaved(win) {
 	let win_rect = win.get_frame_rect();
 	return {
+		id: win.get_id(),
+		hash: windowHash(win),
 		sequence: win.get_stable_sequence(),
 		title: win.get_title(),
 		workspace: win.get_workspace().index(),
@@ -63,6 +66,7 @@ function windowToSaved(win) {
 		y: win_rect.y,
 		width: win_rect.width,
 		height: win_rect.height,
+		occupied: true,
 	}
 }
 
@@ -121,22 +125,63 @@ function saveCurrentWindows() {
 
 //// WINDOW RESTORE
 
-function findSavedWindow(wc, query, similarity) {
-	log('[smart-auto-move] findSavedWindow() - search: ' + wc + ', ' + JSON.stringify(query) + ', ' + similarity);
+function levensteinDistance(a, b) {
+	var m = [], i, j, min = Math.min;
+
+	if (!(a && b)) return (b || a).length;
+
+	for (i = 0; i <= b.length; m[i] = [i++]);
+	for (j = 0; j <= a.length; m[0][j] = j++);
+
+	for (i = 1; i <= b.length; i++) {
+		for (j = 1; j <= a.length; j++) {
+			m[i][j] = b.charAt(i - 1) == a.charAt(j - 1)
+				? m[i - 1][j - 1]
+				: m[i][j] = min(
+					m[i - 1][j - 1] + 1,
+					min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+		}
+	}
+
+	return m[b.length][a.length];
+}
+
+function findSavedWindow(wsh, query, threshold) {
+	log('[smart-auto-move] findSavedWindow() - search: ' + wsh + ', ' + JSON.stringify(query) + ' threshold: ' + threshold);
+
+	if (!savedWindows.hasOwnProperty(wsh)) {
+		log('[smart-auto-move] findSavedWindow() - no such window: ' + wsh)
+		return undefined;
+	}
+
+	let scores = new Map();
+	savedWindows[wsh].forEach(function (sw, swi) {
+		let match_parts = 0;
+		let query_parts = 0;
+		Object.keys(query).forEach(function (key) {
+			let value = query[key];
+			if (key === 'title') {
+				let dist = levensteinDistance(value, sw[key]);
+				match_parts += (value.length - dist) / value.length;
+			} else if (sw[key] === value) {
+				match_parts += 1;
+			}
+			query_parts += 1;
+		});
+		let score = match_parts / query_parts;
+		scores.set(swi, score);
+	});
+
+	let sorted_scores = new Map([...scores.entries()].sort((a, b) => b[1] - a[1]));
+
+	log('[smart-auto-move] findSavedWindow() - sorted_scores: ' + JSON.stringify(Array.from(sorted_scores.entries())));
+
+	let best_swi = sorted_scores.keys().next().value;
+	let best_score = sorted_scores.get(best_swi);
 
 	let found = undefined;
-
-	if (!savedWindows.hasOwnProperty(wc))
-		return undefined;
-
-	savedWindows[wc].forEach(function (sw, swi) {
-		if (sw.sequence === query.sequence) {
-			found = swi;
-		}
-		if (sw.title === query.title) {
-			found = swi;
-		}
-	});
+	if (best_score >= threshold)
+		found = best_swi;
 
 	log('[smart-auto-move] findSavedWindow() - found: ' + found);
 
@@ -146,7 +191,7 @@ function findSavedWindow(wc, query, similarity) {
 function restoreWindow(win) {
 	let wsh = windowSectionHash(win);
 
-	let swi = findSavedWindow(wsh, { title: win.get_title() }, 0.7);
+	let swi = findSavedWindow(wsh, { title: win.get_title(), occupied: false }, WINDOW_RESTORE_MATCH_THRESHOLD);
 
 	if (swi === undefined) return false;
 
@@ -187,6 +232,25 @@ function restoreOrSaveUnseenWindows(app) {
 
 		if (!restoreWindow(win))
 			saveWindow(win);
+	});
+}
+
+function cleanupWindows() {
+	let found = new Map();
+
+	global.get_window_actors().forEach(function (actor) {
+		let win = actor.get_meta_window();
+		found.set(windowHash(win), true);
+	});
+
+	Object.keys(savedWindows).forEach(function (wsh) {
+		let sws = savedWindows[wsh];
+		sws.forEach(function (sw) {
+			if (sw.occupied && !found.has(sw.hash)) {
+				log('[smart-auto-move] cleaning up occupied window: ' + sw.hash)
+				sw.occupied = false;
+			}
+		});
 	});
 }
 
@@ -251,7 +315,7 @@ function disconnectAppSystemSignals(appSys) {
 function disconnectSignals() {
 	log('[smart-auto-move] disconnecting all signals');
 	appSystem.get_running().forEach(function (app) {
-			disconnectAppSignals(app);
+		disconnectAppSignals(app);
 	});
 	disconnectAppSystemSignals(appSystem);
 }
@@ -306,6 +370,7 @@ function handleAppStateChanged(appSys, app) {
 function handleAppWindowsChanged(app) {
 	log('[smart-auto-move] handleAppWindowsChanged(): ' + app.get_name());
 	connectAppSignals(app);
+	cleanupWindows();
 	let timeout = Mainloop.timeout_add(WINDOW_RESTORE_DELAY_MS, function () {
 		restoreOrSaveUnseenWindows(app);
 	});
